375435fc95bace5524c0e698e2afb894242a15e6
Yuya Ohbayashi <yuya9445@gmail.com>
2022-05-23 20:24:15 +0900

    add import

--- zenn.py
+++ zenn.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+#特殊なキーの入力
+from selenium.webdriver.common.keys import Keys
+#セレクトボックスの操作
+from selenium.webdriver.support.ui import Select
+#ホバーを操作
+from selenium.webdriver.common.action_chains import ActionChains
+#明示的な待機
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 #接続
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
