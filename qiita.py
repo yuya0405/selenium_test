65a6a83d8c7a14e2c5671fd410c2bcf538834e0a
Yuya Ohbayashi <yuya9445@gmail.com>
2022-05-23 23:43:52 +0900

    third test

--- qiita.py
+++ qiita.py
@@ -1,9 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service as fs
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_service = fs.Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=chrome_service)
+
 driver.get('https://www.google.com/')
 time.sleep(1)
 search_box = driver.find_element(by=By.NAME, value="q")
