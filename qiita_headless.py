dbf121844a983f2316a16e2977301c1a34ca8027
Yuya Ohbayashi <yuya9445@gmail.com>
2022-05-24 13:13:27 +0900

    fifth test

--- qiita_headless.py
+++ qiita_headless.py
@@ -21,8 +21,19 @@ print(driver.title)
 for g_h3 in driver.find_elements(by=By.CSS_SELECTOR, value=".g h3"):
     print(g_h3.text)
 
+# Googleで右側にも検索結果が出たらエラー？？？
+# 参考：https://qiita.com/y_sayama/items/ed0558c891db36fea83c
+
 # stats = driver.find_element(by=By.ID, value="resultStats").text
 # print(stats)
+
+# for i, g in enumerate(driver.find_elements(By.CLASS_NAME, "g")):
+#     print("------ " + str(i+1) + " ------")
+#     r = g.find_element(By.CLASS_NAME, "r")
+#     print(r.find_element(By.TAG_NAME, "h3").text)
+#     print("\t" + r.find_element(By.TAG_NAME, "a").get_attribute("href"))
+#     s = g.find_element(By.CLASS_NAME, "s")
+#     print("\t" + s.find_element(By.CLASS_NAME, "st").text)
 
 driver.save_screenshot('search_results.png')
 driver.quit()
